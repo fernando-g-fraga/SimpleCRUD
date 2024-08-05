79edd0934c01c14caa46d599123a8c25fda3ab4e
Fernando <fernandogrfraga@gmail.com>
2024-08-05 18:32:06 -0300

    Added Create Record route

--- main.go
+++ main.go
@@ -12,11 +12,30 @@ Desenvolvido por Fernando Fraga para fins de portfólio.
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
+type contato struct {
+	name   string
+	email  string
+	phone  string
+	status bool
+}
+
 func main() {
+
+	db := ConectarDB()
+	CreateTable(db)
+
+	err := db.Ping(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	escolha := showmenu()
 
@@ -24,7 +43,8 @@ func main() {
 	default:
 		os.Exit(1)
 	case 1:
-		createRecord()
+		rows := createRecord(db)
+		fmt.Println(rows)
 	case 2:
 		selectAll()
 	case 3:
@@ -37,7 +57,31 @@ func main() {
 
 }
 
-func createRecord() {}
+func createRecord(db *pgx.Conn) pgx.Rows {
+
+	var first string
+	var email string
+	var phone string
+	status := true
+
+	fmt.Println("Digite o nome do seu contato: ")
+	fmt.Scan(&first)
+	fmt.Println("Digite o email: ")
+	fmt.Scan(&email)
+	fmt.Println("Digite o telefone: ")
+	fmt.Scan(&phone)
+
+	query := `INSERT INTO contato (name,email,phone,status)
+	VALUES ($1,$2,$3,$4) returning ID`
+	rows, err := db.Query(context.Background(), query, first, email, phone, status)
+
+	if err != nil {
+		log.Fatal("Ocorreu um erro ao cadastrar o usuário ", err)
+	}
+
+	return rows
+
+}
 func selectAll()    {}
 func selectOne()    {}
 func deleteRecord() {}
@@ -57,4 +101,31 @@ func showmenu() int {
 
 	return escolha
 
+}
+
+func ConectarDB() *pgx.Conn {
+	postgreURL := "postgres://postgres:secret@localhost:5432/simpleCRUD"
+	db, err := pgx.Connect(context.Background(), postgreURL)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	return db
+
+}
+
+func CreateTable(db *pgx.Conn) {
+	query := `CREATE TABLE IF NOT EXISTS contato (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(20),
+	email VARCHAR(20),
+	phone VARCHAR(20),
+	status BOOLEAN
+	)`
+
+	_, err := db.Exec(context.Background(), query)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
